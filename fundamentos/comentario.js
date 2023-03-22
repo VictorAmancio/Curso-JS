ac562548a2c71434100f016a7011280338082ea8
Victor <victor_amancio@hotmail.com>
2023-03-22 14:03:33 -0300

    Criação da Pasta Fundamentos

---
+++ fundamentos/comentario.js
@@ -0,0 +1,15 @@
+// É assim que se faz o comentario em JS, comentario de uma linha
+console.log('Linha 1')
+
+/*
+Comentario de
+Multiplas Linhas
+*/
+console.log('Linha 2')
+
+/*
+* Esse 
+*é 
+*um Exemplo
+*/
+console.log('Linha3')
---
+++ fundamentos/variaveisEConstantes.js
@@ -0,0 +1,15 @@
+var a /*identificador colocar o nome*/ = 3
+let b = 4
+
+var a = 30
+b = 40
+
+console.log(a,b)
+
+a = 300
+b = 400
+console.log(a,b)
+
+const c = 5
+// c = 50
+console.log(c)
